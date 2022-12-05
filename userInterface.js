9d9dd225535b422971243d9702e7ba04714b8888
ckwatt11 <ckwatt11@gmail.com>
2022-12-05 16:17:33 -0500

    initialize ui step 1

--- userInterface.js
+++ userInterface.js
@@ -1,5 +1,15 @@
 // Code for setting up the UI of the application. 
 
+function initializeInterface(vidStream){
+
+    // Initialize state logic for buttons w/ JQuery.
+
+    buttons = ['#video-btn', '#screen-share-btn', '#mic-btn', '#exit-btn']; 
+
+    for (btn in buttons){
+        $(`${btn}`).prop("disabled", false);
+    }
 
 
-function initializeInterface()
+
+}
