84cc04a8f38835604095888390143c2ad02efe05
ckwatt11 <ckwatt11@gmail.com>
2022-12-05 16:31:26 -0500

    hmm

--- userInterface.js
+++ userInterface.js
@@ -1,5 +1,21 @@
 // Code for setting up the UI of the application. 
 
+
+// functions and event handlers for buttons and other controls. 
+
+function toggleButton(button){{
+    button.toggleClass('btn-dark').toggleClass('btn-danger'); // mainly for the video and mic buttons 
+}
+
+function toggleScreenShareButton(){
+    
+}
+    
+}
+
+
+
+
 function initializeInterface(vidStream){
 
     // Initialize state logic for buttons w/ JQuery.
