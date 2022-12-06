807416c449e93bb5a20741ce8932ed7b163c05a0
ckwatt11 <ckwatt11@gmail.com>
2022-12-06 04:17:21 -0500

    add more toggle event handlers

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-/* All Agora SDK event listeners must be at the top level. */
+/* Main Javascript logic for interacting with and handling the UI .*/
+
+// All Agora SDK event listeners must be at the top level. 
+
+
+let client = AgoraRTC.createClient({mode:'rtc', codec:"vp8"}); 
+let remoteClient = ({mode: 'rtc', codec: "vp8"});
+
+let hostId = "";
+let isScrSharing = false; 
+ 
+var cameraResolution = '600_4'; // quality of user's camera stream : 800 x 600 (?)
+var screenResolution = '600_2'; // quality of other streams on user's screen 
+
+var appId = "";
+var channelId = 'ckwatt11-webchat';
+
+let localTracks = {};
--- userInterface.js
+++ userInterface.js
@@ -1,22 +1,24 @@
+
+
 // Code for setting up the UI of the application. 
 
 
 // functions and event handlers for buttons and other controls. 
 
 function toggleButton(button){{
-    button.toggleClass('btn-dark').toggleClass('btn-danger'); // mainly for the video and mic buttons 
+    button.toggleClass('btn-dark').toggleClass('btn-danger'); // mainly for the (colors of) video and mic buttons. 
 }
 
-function toggleScreenShareButton(){
+function toggleMicrophone(stream) {  //
+
+}
     
 }
-    
-}
 
 
 
 
-function initializeInterface(vidStream){
+function initializeInterface(stream){
 
     // Initialize state logic for buttons w/ JQuery.
 
